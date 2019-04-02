f5c257338f09e1e73ee315c367ccee20788ff909
Jamal Shareef <theepie@hotmail.com>
2019-04-02 06:21:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.size == 1
     array.join
-  if array.size == 2
+  elsif array.size == 2
     array[array.size-1] = "and " + array.last
     array.join(" ")
   else
