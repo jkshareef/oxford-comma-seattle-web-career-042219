a5d9acafb14ecebacac78acaa2c5b37f31811cf5
Jamal Shareef <theepie@hotmail.com>
2019-04-02 06:20:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
-  array[array.size-1] = "and " + array.last
-  array.join(", ")
+  if array.size == 1
+    array.join
+  if array.size == 2
+    array[array.size-1] = "and " + array.last
+    array.join(" ")
+  else
+    array[array.size-1] = "and " + array.last
+    array.join(", ")
 end
